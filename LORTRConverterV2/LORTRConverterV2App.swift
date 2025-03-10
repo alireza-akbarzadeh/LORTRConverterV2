68f4aae40da873d46634926a336b1308774a427d
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2025-03-10 22:58:22 +0330

    Initial Commit

---
+++ LORTRConverterV2/LORTRConverterV2App.swift
@@ -0,0 +1,17 @@
+//
+//  LORTRConverterV2App.swift
+//  LORTRConverterV2
+//
+//  Created by ALIREZA AKBARZADEH  on 12/20/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct LORTRConverterV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
