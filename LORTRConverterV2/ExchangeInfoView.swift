0cb84a9f751d46d9778ea4d6ae2848cb860d84b8
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2025-03-13 11:32:46 +0330

    creat setup for the lov converter app

---
+++ LORTRConverterV2/ExchangeInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfoView.swift
+//  LORTRConverterV2
+//
+//  Created by ALIREZA AKBARZADEH  on 12/23/1403 AP.
+//
+
+import SwiftUI
+
+struct ExchangeInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfoView()
+}
---
+++ LORTRConverterV2/components/ExchangeRate.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeRate.swift
+//  LORTRConverterV2
+//
+//  Created by ALIREZA AKBARZADEH  on 12/23/1403 AP.
+//
+
+import SwiftUI
+
+struct ExchangeRate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeRate()
+}
